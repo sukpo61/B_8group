e9ec2db1bfeb8a967b07b4781cbf9ab3fc687e3c
marina <111960712+nyangmal123@users.noreply.github.com>
2022-11-02 15:36:17 +0900

    Add files via upload
    초안

--- app.py
+++ app.py
@@ -2,31 +2,36 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:go3355@cluster0.vqsgnyr.mongodb.net/Cluster0?retryWrites=true&w=majority')
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.1xtz0yo.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
-
 
 @app.route('/')
 def home():
-    return render_template('main.html')
+    return render_template('mainpage.html')
 
-@app.route("/homework", methods=["POST"])
+@app.route("/mari")
+def mari():
+    return render_template('mari.html')
+
+
+@app.route("/mari", methods=["POST"])
 def homework_post():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
 
-    doc = {'name': name_receive,
-           'comment': comment_receive}
-    db.comments.insert_one(doc)
-
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+    db.marina.insert_one(doc)
     return jsonify({'msg':'POST 연결 완료!'})
 
-@app.route("/homework", methods=["GET"])
+@app.route("/mari", methods=["GET"])
 def homework_get():
-
-    fan_list = list(db.comments.find({}, {'_id': False}))
-
-    return jsonify({'fans': fan_list})
+    comment_list = list(db.mari.find({},{'_id':False}))
+    return jsonify({'comments':comment_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5022, debug=True)
