26365afc7e645790c56b57ee34f129e3e95ce42c
Hyeon <sukpo6010@gmail.com>
2022-11-02 11:59:33 +0900

    4

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('main.html')
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
